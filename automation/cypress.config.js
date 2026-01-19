4b58ec0018acbb514a903651823e6180764b10f5
letienson987 <itsnhim.30042004@gmail.com>
2026-01-20 00:04:40 +0700

    feat: add beautiful HTML test reports with Mochawesome

--- automation/cypress.config.js
+++ automation/cypress.config.js
@@ -8,7 +8,16 @@ module.exports = defineConfig({
     viewportHeight: 1000,
     pageLoadTimeout: 120000, // Increased timeout for slow loading pages
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       return config;
+    },
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      html: true,
+      json: true,
+      charts: true,
     },
   },
 });
